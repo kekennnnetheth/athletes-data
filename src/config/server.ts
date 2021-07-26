f4f58c9afda03258691b183e4fddab5ff6528282
Kenneth <kenneth_chua@mymail.sutd.edu.sg>
2021-07-26 23:33:16 +0800

    Add more endpoints

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,10 +1,16 @@
-import express from 'express';
+import express from "express";
+import path from "path";
 
-const app = express()
+const app = express();
 const PORT: string | number = process.env.PORT || 5000;
 
-app.use("*", (req, res) => {
-  res.send("<h1>Welcome to your simple server! Awesome right</h1>")
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to your simple server! Awesome right</h1>");
 });
+
+app.get("/overview", (req, res) =>
+  res.send("<h1>This will be the overview result page</h1>")
+);
 
 app.listen(PORT, () => console.log(`hosting @${PORT}`));
