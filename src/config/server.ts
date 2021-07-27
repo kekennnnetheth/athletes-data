ba1c79e3df3702f0c7ebb09e9c21682a7d789ed7
Kenneth <kenneth_chua@mymail.sutd.edu.sg>
2021-07-27 23:40:33 +0800

    Try adding db

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,16 +1,32 @@
 import express from "express";
 import path from "path";
+import { Pool } from "pg";
 
 const app = express();
 const PORT: string | number = process.env.PORT || 5000;
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to your simple server! Awesome right</h1>");
 });
 
-app.get("/overview", (req, res) =>
-  res.send("<h1>This will be the overview result page</h1>")
-);
+app.get("/overview", async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM user");
+    const results = { results: result ? result.rows : null };
+    res.render("pages/overview", results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
 
 app.listen(PORT, () => console.log(`hosting @${PORT}`));
