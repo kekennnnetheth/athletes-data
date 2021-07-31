ee80e6bbb703c14705905749fdd090953e0ec285
Kenneth <kenneth_chua@mymail.sutd.edu.sg>
2021-07-31 17:06:54 +0800

    Setup view engine

--- src/config/server.ts
+++ src/config/server.ts
@@ -12,6 +12,8 @@ const pool = new Pool({
 });
 
 app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to your simple server! Awesome right</h1>");
 });
@@ -22,7 +24,7 @@ app.get("/overview", async (req, res) => {
     const result = await client.query("SELECT * FROM user");
     const results = { results: result ? result.rows : null };
     res.send(results);
-    // res.render("pages/overview", results);
+    res.render("pages/overview", results);
     client.release();
   } catch (err) {
     console.error(err);
